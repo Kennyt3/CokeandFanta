5d755cdcb78c2ce4316d64537feecf1f1914cb0e
Mercy <kennyt341@gmail.com>
2024-06-29 06:31:18 +0800

    added gallery

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,4 +1,4 @@
-import Gallery from 'react-photo-gallery'
+import { Gallery } from 'react-grid-gallery'
 import Link from 'next/link'
 import Image from 'next/image'
 import Telcode from './telcode'
@@ -11,69 +11,33 @@ export default function PicGallery() {
   const photos = [
     {
       src: '/engagement/coke1.jpg',
-      width: 4,
-      height: 3,
+      width: 500,
+      height: 600,
     },
     {
       src: '/engagement/coke2.jpg',
-      width: 1,
-      height: 1,
+      width: 500,
+      height: 700,
     },
     {
       src: '/engagement/coke3.jpg',
-      width: 3,
-      height: 4,
+      width: 200,
+      height: 300,
     },
     {
       src: '/engagement/coke4.jpg',
-      width: 3,
-      height: 4,
+      width: 2500,
+      height: 3500,
     },
     {
       src: '/engagement/coke5.jpg',
-      width: 3,
-      height: 4,
-    },
-    {
-      src: '/engagement/coke6.jpg',
-      width: 4,
-      height: 3,
-    },
-    {
-      src: '/engagement/coke3.jpg',
-      width: 3,
-      height: 4,
-    },
-    {
-      src: '/engagement/coke4.jpg',
-      width: 4,
-      height: 3,
-    },
-    {
-      src: '/engagement/coke5.jpg',
-      width: 4,
-      height: 3,
-    },
-    {
-      src: '/engagement/coke6.jpg',
-      width: 3,
-      height: 4,
-    },
-    {
-      src: '/engagement/coke6.jpg',
-      width: 4,
-      height: 3,
-    },
-    {
-      src: '/engagement/coke6.jpg',
-      width: 4927,
-      height: 1000,
-    },
-    {
-      src: '/engagement/coke6.jpg',
-      width: 1,
-      height: 1,
+      width: 2500,
+      height: 3500,
     },
   ]
-  return <Gallery photos={photos} />
+  return (
+    <div>
+      <Gallery images={photos} rowHeight={500} />
+    </div>
+  )
 }
